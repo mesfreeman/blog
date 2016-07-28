b9758f56a515795038364e9e4d619cd4ca640bd1
Double <mesfreeman@126.com>
2016-07-28 10:04:21 +0800

    优化

--- usr/themes/jianshu-master/header.php
+++ usr/themes/jianshu-master/header.php
@@ -38,6 +38,7 @@ $font_mode = Typecho_Cookie::get('font-mode', '');
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
+    <link rel="shortcut icon" href="<?php $this->options->themeUrl('img/freeman.png'); ?>" type="image/png">
     <link rel="stylesheet" href="http://apps.bdimg.com/libs/fontawesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('css/style.css'); ?>">
 	<?php if($this->options->iconCss):?>
