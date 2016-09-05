9a3b547b7f373a33e7d4976244f55989d6cddae9
Double <mesfreeman@126.com>
2016-09-05 09:39:07 +0800

    文章内容页加入点赞数量显示

--- usr/themes/jianshu-master/post.php
+++ usr/themes/jianshu-master/post.php
@@ -15,9 +15,9 @@
 		</div>
         <h1 class="post-title" itemprop="name headline"><?php $this->title() ?></h1>
         <ul class="post-meta clearfix">
-            <li><?php _e('阅读');Views_Plugin::theViews('',''); ?></li>
-            <li><?php $this->commentsNum('评论%d'); ?></li>
-			<li><?php _e('喜欢');$this->likesNum(); ?></li>
+            <li><?php _e('阅读');Views_Plugin::theViews(':(',')'); ?></li>
+            <li><?php $this->commentsNum('评论:(%d)'); ?></li>
+			<li><?php _e('赞');Views_Plugin::theLikes(':(',')'); ?></li>
         </ul>
         <div class="post-content" itemprop="articleBody">
             <?php parseContent($this); ?>
